331cb44584a7a3145ba5180e50f551d60214df5c
Arvind Vishwakarma <arvind.vishwakarma@vernost.in>
2023-04-16 16:35:06 +0530

    mail setup

--- model/bookCheckIn.js
+++ model/bookCheckIn.js
@@ -1,10 +1,10 @@
 
-// const emailNotification = require('./sendEmail');
+const emailNotification = require('./sendEmail');
 module.exports.bookAppointment = async function (req, res) {
     try {
         req = req.body
         console.log("Requeeuususuus", req)
-        // let sendEmail = await emailNotification.sendEmail(req)
+        let sendEmail = await emailNotification.sendEmail(req)
         return { status: true, code: 200, message: "OK" }
     } catch (error) {
         console.log("Errororor", error)
